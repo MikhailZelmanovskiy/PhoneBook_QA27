fedc9ab30860716b917a4741c03253e7a06b3c2f
MikhailZelmanovskiy <strayn.mike@gmail.com>
2021-04-04 16:16:56 +0300

    homework

--- src/test/java/LogOutTest.java
+++ src/test/java/LogOutTest.java
@@ -1,13 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class LogOutTest extends TestBase{
     @BeforeMethod
     public void preconditions(){
-        //login
+        wd.findElement(By.linkText("LOGIN")).click();
+        List<WebElement> inputs = wd.findElements(By.tagName("input"));
+        fillByElement(inputs.get(0), "strayn.mike@gmail.com");
+        fillByElement(inputs.get(1), "Mikhail-1988");
+        List<WebElement> buttons = wd.findElements(By.tagName("button"));
+        buttons.get(0).click();
     }
     @Test
     public void logOutTest(){
+        wd.findElement(By.tagName("button")).click();
+        List<WebElement> tagA = wd.findElements(By.tagName("a"));
+        String text = tagA.get(2).getText();
+        Assert.assertEquals(text, "LOGIN");
 
     }
 }
--- src/test/java/Registration.java
+++ src/test/java/Registration.java
@@ -1,4 +1,9 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class Registration extends TestBase{
     @Test
@@ -7,5 +12,14 @@ public class Registration extends TestBase{
         int index = (int)((System.currentTimeMillis()/1000)%3600);
         String email = "Tom" + index + "@gmail.com";
         String password = "Tom-" +index;
+        wd.findElement(By.linkText("LOGIN")).click();
+        List<WebElement> inputs = wd.findElements(By.tagName("input"));
+        fillByElement(inputs.get(0), email);
+        fillByElement(inputs.get(1), password);
+        List<WebElement> buttons = wd.findElements(By.tagName("button"));
+        buttons.get(1).click();
+        pause(5000);
+        String text = wd.findElement(By.tagName("button")).getText();
+        Assert.assertEquals(text,"Sign Out");
     }
 }
